b98c68782d089eb42b28085028a2e05192b4cf2e
mikolajchm <chmielewski.5454@gmail.com>
2024-01-16 14:34:53 +0100

    Task 12.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Button from './components/Button/Button';
+import Timer from './components/Timer/Timer';
 
 function App() {
+  
+  
+  const [time, setTime] = useState(0);
+  const [intervalValue, setIntervalValue] = useState(null);
+
+  const startTimer = () => {
+    if (intervalValue) clearInterval(intervalValue);
+    setIntervalValue(setInterval(() => { setTime(prevValue => prevValue + 1) }, 1));
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalValue);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (intervalValue) clearInterval(intervalValue);
+    };
+  }, [intervalValue]);
+
+  const resetTimer = () => {
+    if (intervalValue) clearInterval(intervalValue);
+    setTime(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Timer Timer={time} />
+      <Button onClick={startTimer}>Start</Button>
+      <Button onClick={stopTimer}>Stop</Button>
+      <Button onClick={resetTimer}>Reset</Button>
     </div>
   );
 }
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,26 @@
+import styles from './Timer.module.scss';
+
+const Timer = ({ Timer }) => {
+    const padTo2Digits = num => {
+        return num.toString().padStart(2, '0');
+      }
+      
+      const convertMsToTime = milliseconds => {
+        let seconds = Math.floor(milliseconds / 250);
+        let minutes = Math.floor(seconds / 60);
+        let hours = Math.floor(minutes / 60);
+      
+        milliseconds = milliseconds % 1000;
+        seconds = seconds % 60;
+        minutes = minutes % 60;
+        
+        let formatedTime = `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(seconds)}.${padTo2Digits(milliseconds)}`;
+        return formatedTime;
+    };
+    return (
+        <div className={styles.time}>
+            {convertMsToTime(Timer)}
+        </div>
+    )
+}  
+export default Timer;
